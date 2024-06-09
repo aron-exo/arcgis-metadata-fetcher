b1381971ae7cd47a2268dce7727017b26b24083c
aron-exo <166415369+aron-exo@users.noreply.github.com>
2024-06-09 13:03:04 +0300

    Update search_servers.py
    assinging gis password and username

--- search_servers.py
+++ search_servers.py
@@ -5,7 +5,7 @@ from arcgis.geocoding import reverse_geocode
 from urllib.parse import urlparse, urlunparse
 
 # Initialize the GIS
-gis = GIS("home")
+gis = GIS("https://www.arcgis.com", os.getenv('USERNAME'), os.getenv('PASSWORD'))
 
 # Define the place of interest
 county_name = "Los Angeles County, California, USA"
